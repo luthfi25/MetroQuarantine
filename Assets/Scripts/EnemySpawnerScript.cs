b0c797ab1ab1de1c66f7085a0de214dc9594fa73
Muhammad Luthfi <luthfi.muhammad825@gmail.com>
2020-05-27 17:42:47 +0900

    Edit Title Scene, Initiate RTH Scene

---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public List<Transform> SpawnPositions;
+    public List<RuntimeAnimatorController> NPCAnimations;
+    public GameObject NPCPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(RuntimeAnimatorController npcAnim in NPCAnimations){
+            int randIndex = Random.Range(0, SpawnPositions.Count);
+            GameObject npc = Instantiate(NPCPrefab, SpawnPositions[randIndex].position, transform.rotation);
+            
+            Animator npcAnimator;
+            if(npc.TryGetComponent<Animator>(out npcAnimator)){
+                npcAnimator.runtimeAnimatorController = npcAnim;
+            }
+
+            SpawnPositions.RemoveAt(randIndex);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    public Sprite FaceDown;
+    public Sprite FaceUp;
+    public Sprite FaceLeft;
+    public Sprite FaceRight;
+
+    readonly string[] ORIENTATION = {"down", "up", "left", "right"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int initOrientationIndex = Random.Range(0, ORIENTATION.Length);
+        string initOrientation = ORIENTATION[initOrientationIndex];
+        ChangeOrientation(initOrientation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeOrientation(string orientationVal){
+        SpriteRenderer spriteRenderer;
+
+        if(TryGetComponent<SpriteRenderer>(out spriteRenderer)){
+            switch(orientationVal){
+                case "down":
+                    spriteRenderer.sprite = FaceDown;
+                    break;
+                case "up":
+                    spriteRenderer.sprite = FaceUp;
+                    break;
+                case "left":
+                    spriteRenderer.sprite = FaceLeft;
+                    break;
+                case "right":
+                    spriteRenderer.sprite = FaceRight;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
